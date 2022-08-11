f5cf66cc9643c3ef654ffefbb6080d7bc0a9a580
andreaskundig <andreas.kuendig@gmx.ch>
2022-08-11 11:37:34 +0200

    update experiences (homePath, more translations)

--- experiences/_nuxt/a3fba43.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[170],{904:function(e){e.exports=JSON.parse('{"experiences":["tracker-control","twitter"],"i18nLocales":["fr"],"i18nLocale":"fr","theme":{"primary":"#0C2D48","secondary":"#2E8BC0"}}')}}]);
