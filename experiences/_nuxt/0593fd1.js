fa0467939409f2d6ef89148a2345531242d2f4d7
andreaskundig <andreas.kuendig@gmx.ch>
2022-08-15 14:01:18 +0200

    update experiences: more translations (working)

---
+++ experiences/_nuxt/0593fd1.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[207],{904:function(e){e.exports=JSON.parse('{"experiences":["tracker-control","twitter"],"i18nLocales":["fr"],"i18nLocale":"fr","homePath":"https://hestia.ai","theme":{"primary":"#0C2D48","secondary":"#2E8BC0"}}')}}]);
--- experiences/_nuxt/1dfb080.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[170],{904:function(e){e.exports=JSON.parse('{"experiences":["tracker-control","twitter"],"i18nLocales":["fr"],"i18nLocale":"fr","homePath":"https://hestia.ai","theme":{"primary":"#0C2D48","secondary":"#2E8BC0"}}')}}]);
