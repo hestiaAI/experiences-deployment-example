3a6fb8ee502853d088caf71e40a25186af725ec3
andreaskundig <andreas.kuendig@gmx.ch>
2022-11-14 18:34:16 +0100

    first build with modularisation

---
+++ experiences/_nuxt/b5f456c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[46],{1333:function(e){e.exports=JSON.parse('{"experiences":["tracker-control","twitter"],"i18nLocales":["fr"],"i18nLocale":"fr","homePath":"https://hestia.ai","theme":{"primary":"#0C2D48","secondary":"#2E8BC0"}}')}}]);
